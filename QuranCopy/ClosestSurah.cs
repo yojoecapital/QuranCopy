c271279c180ad7ad6a9e6be4bb37c6e88fbb0454
yojoecapital <yousefsuleiman10@gmail.com>
2023-08-29 12:28:40 -0500

    fix-repo

---
+++ QuranCopy/ClosestSurah.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace QuranCopy
+{
+    public static class ClosestSurah
+    {
+        private static int? FindClosestSurah(IEnumerable<XElement> surahs, string key)
+        {
+            int min = int.MaxValue;
+            int? result = null;
+            foreach (XElement surah in surahs)
+            {
+                string s = (string)surah.Attribute("TransliterationName");
+                int distance = ComputeLevenshteinDistance(s, key);
+                if (distance < min)
+                {
+                    min = distance;
+                    result = int.Parse((string)surah.Attribute("SurahId"));
+                }
+            }
+            return result;
+        }
+
+        private static int ComputeLevenshteinDistance(string s, string t)
+        {
+            int length = s.Length;
+            int length2 = t.Length;
+            int[,] array = new int[length + 1, length2 + 1];
+            for (int i = 0; i <= length; i++)
+            {
+                array[i, 0] = i;
+            }
+            for (int j = 0; j <= length2; j++)
+            {
+                array[0, j] = j;
+            }
+            for (int j = 1; j <= length2; j++)
+            {
+                for (int i = 1; i <= length; i++)
+                {
+                    int min = ((s[i - 1] != t[j - 1]) ? 1 : 0);
+                    array[i, j] = Math.Min(Math.Min(array[i - 1, j] + 1, array[i, j - 1] + 1), array[i - 1, j - 1] + min);
+                }
+            }
+            return array[length, length2];
+        }
+
+        public static XElement GetSurah(string arg)
+        {
+            int surahNumber;
+            if (int.TryParse(arg, out surahNumber))
+            {
+                if (surahNumber > 0 && surahNumber <= 114)
+                {
+                    return FileManager.Surahs.FirstOrDefault((XElement row) => int.Parse((string)row.Attribute("SurahId")) == surahNumber);
+                }
+                return null;
+            }
+            var surahs = FileManager.Surahs;
+            surahNumber = FindClosestSurah(surahs, arg).Value;
+            return surahs.FirstOrDefault((XElement row) => int.Parse((string)row.Attribute("SurahId")) == surahNumber);
+        }
+    }
+}
---
+++ QuranCopy/SearchSettings.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace QuranCopy 
+{
+    public class SearchSettings
+    {
+        public int peek;
+        public int resultsPerPage;
+        public bool ignoreAccents;
+        public bool useArabize;
+        public string arabizePath;
+        public Dictionary<string, string> replace;
+
+        public SearchSettings() { }
+
+        public static SearchSettings Create()
+        {
+            var json = File.ReadAllText(FileManager.SettingsFilePath);
+            var settings = JsonConvert.DeserializeObject<SearchSettings>(json);
+            FileManager.ArabizePath = settings.arabizePath;
+            if (settings.replace == null) 
+                settings.replace = new Dictionary<string, string>();
+            return settings;
+        }
+    }
+
+}
