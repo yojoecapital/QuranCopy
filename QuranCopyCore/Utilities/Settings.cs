f623420f0b519e271286b2eb4d72dfb68b13d994
Yousef Suleiman <yousefsuleiman10@gmail.com>
2024-05-22 14:19:13 -0500

    updates

--- QuranCopyCore/Utilities/Settings.cs
+++ QuranCopyCore/Utilities/Settings.cs
@@ -4,11 +4,11 @@ namespace QuranCopyCore.Utilities
 {
     internal class Settings
     {
-        public int peek;
+        public int peek = 1;
         public int resultsPerPage = 1;
-        public bool ignoreAccents;
-        public bool useArabize;
-        public string arabizePath;
+        public bool ignoreAccents = false;
+        public bool useArabize = false;
+        public string arabizePath = string.Empty;
         public Dictionary<string, string> replace;
     }
 
